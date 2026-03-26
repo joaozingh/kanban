376537680cb0faf5891e5904b062c405a6388735
Joaozingh <joaovictorpsasousa@gmail.com>
2026-03-25 23:54:42 -0300

    subindo

---
+++ parte.js
@@ -0,0 +1,91 @@
+//Drag and Drop: arrastar cards entre colunas 
+let cardArrastado = null;
+
+//criar card
+function criarCard(colunaId) {
+    const texto = prompt("Digite o texto do card:");
+    if (!texto) return;
+
+    const card = document.createElement("div"); //cria o card
+    card.className = "card"; // aplica estilo
+    card.textContent = texto;
+    card.draggable = true; //permite arrastar 
+
+    //conteudo do card
+    card.innerHTML = `
+    <span class ="texto">${texto}</span>
+    <div class="acoes">
+     <button class="editar">✏</button>
+     <button class="excluir">❌</button>
+      </div>
+     `
+
+    adicionarEventos(card);
+
+    document.getElementById(colunaId).appendChild(card); //appenChild coloca na coluna
+}
+//eventos do card
+function adicionarEventos(card) {
+    // comçar a arrastar 
+    card.addEventListener("dragstart" , (e) => {  //dragstart: quando começar a arrastar guarda o card numa varíavel
+        cardArrastado = card;
+    });
+ 
+    //editar texto
+    card.addEventListener("dblclick", () => { //dblclick: editar o texto 
+        const novoTexto = prompt("Editar card:", card.textContent);
+        if (novoTexto) card.textContent = novoTexto;
+    });
+   
+     const texto = card.querySelector(".texto");
+     const btnEditar = card.querySelector(".editar");
+     const btnExcluir = card.querySelector(".excluir");
+       
+    // excluir
+     btnExcluir.addEventListener("click", () => {
+     card.remove();
+  });
+
+}
+
+//Permitir soltar nas colunas
+document.querySelectorAll(".coluna").forEach(coluna => {
+
+    coluna.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    });
+
+    coluna.addEventListener("drop", () => {
+        if (cardArrastado){
+        coluna.appendChild(cardArrastado);
+        cardArrastado = null;
+        }
+    });
+});
+
+function salvarEstado() {
+  const dados = {};
+
+  document.querySelectorAll(".coluna").forEach(coluna => {
+    const id = coluna.id;
+    dados[id] = [];
+
+    coluna.querySelectorAll(".card .texto").forEach(card => {
+      dados[id].push(card.textContent);
+    });
+  });
+
+  localStorage.setItem("kanban", JSON.stringify(dados));
+}
+
+function carregarEstado() {
+  const dados = JSON.parse(localStorage.getItem("kanban"));
+  if (!dados) return;
+
+  for (let colunaId in dados) {
+    dados[colunaId].forEach(texto => {
+      criarCardComTexto(colunaId, texto);
+    });
+  }
+}
+carregarEstado();
